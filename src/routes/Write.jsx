871ab8f07f107562b98efd795d74d0a52205d328
Karani Wachira <wachirakeith@gmail.com>
2025-01-10 18:18:06 +0300

    fix: edited write.jsx route

--- src/routes/Write.jsx
+++ src/routes/Write.jsx
@@ -19,19 +19,19 @@ const Write = () => {
             <h1 className='text-xl font-light'>Create a new post</h1>
             <form className='flex flex-col gap-6 flex-1 mb-6'>
                 <button className='w-max p-2 shadow-md rounded-xl text-sm text-gray-500 bg-white'>Add a cover image</button>
-            <input className="text-4xl font-semibold bg-transparent outline-none" type="text" placeholder="New post title" />
-            <div className="flex items-center gap-4">
-                <label htmlFor="" className="text-sm"> Choose a category:</label>
-                <select name="cat" id="" className="p-2 rounded-xl bg-white shadow-md">
-                    <option value="general ">General</option>
-                    <option value="baseball">Baseball</option>
-                    <option value="softball">Softball</option>
-                    <option value="baseball5">Baseball5</option>
-                    <option value="little-league">Little league</option>
-                </select>
-            </div>
-            <textarea className="p-4 rounded-xl bg-white shadow-md" name="desc" placeholder="a short description" />
-                    <ReactQuill theme="snow" className="flex-1 rounded-xl bg-white shadow-md"/>
+                <input className="text-4xl font-semibold bg-transparent outline-none" type="text" placeholder="New post title" />
+                <div className="flex items-center gap-4">
+                    <label htmlFor="" className="text-sm"> Choose a category:</label>
+                    <select name="cat" id="" className="p-2 rounded-xl bg-white shadow-md">
+                        <option value="general ">General</option>
+                        <option value="baseball">Baseball</option>
+                        <option value="softball">Softball</option>
+                        <option value="baseball5">Baseball5</option>
+                        <option value="little-league">Little league</option>
+                    </select>
+                </div>
+                <textarea className="p-4 rounded-xl bg-white shadow-md" name="desc" placeholder="a short description" />
+                <ReactQuill theme="snow" className="flex-1 rounded-xl bg-white shadow-md"/>
                 <button className="bg-blue-800 text-white font-medium rounded-xl mt-2 p-2 w-36">Send</button>
             </form>
         </div>
